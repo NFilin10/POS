d4de68029a49a2a5c01ff94f39684475c5b09d8c
Nikita Filin <nikita10filin@gmail.com>
2023-10-17 02:51:55 +0300

    adding and deleting team members methods added, close #2

--- SaleSystemGUI/src/main/java/ee/ut/math/tvt/salessystem/ui/controllers/TeamTabController.java
+++ SaleSystemGUI/src/main/java/ee/ut/math/tvt/salessystem/ui/controllers/TeamTabController.java
@@ -2,7 +2,9 @@ package ee.ut.math.tvt.salessystem.ui.controllers;
 
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceDialog;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
@@ -10,8 +12,7 @@ import javafx.scene.text.Text;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.util.Properties;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class TeamTabController implements Initializable {
     @FXML
@@ -29,6 +30,8 @@ public class TeamTabController implements Initializable {
     @FXML
     private ImageView logo;
 
+    private List<String> membersList = new ArrayList<>();
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -40,10 +43,15 @@ public class TeamTabController implements Initializable {
             Properties properties = new Properties();
             properties.load(input);
 
+            String members = properties.getProperty("members");
+            String[] memberArray = members.split(", ");
+            membersList.addAll(Arrays.asList(memberArray));
+
             teamName.setText(properties.getProperty("name"));
             teamLeader.setText(properties.getProperty("contactPerson"));
             teamLeaderMail.setText(properties.getProperty("contact"));
             teamMembers.setText(properties.getProperty("members"));
+
 
             String logoPath = properties.getProperty("logo");
             logo.setImage(new Image(logoPath));
@@ -51,4 +59,41 @@ public class TeamTabController implements Initializable {
             e.printStackTrace();
         }
     }
+
+
+    @FXML
+    private void addMember() {
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("Add Member");
+        dialog.setHeaderText("Enter the member's name:");
+        dialog.setContentText("Name:");
+
+        Optional<String> result = dialog.showAndWait();
+        result.ifPresent(name -> {
+            membersList.add(name);
+            updateTeamMembers();
+        });
+    }
+
+    @FXML
+    private void removeMember() {
+        ChoiceDialog<String> dialog = new ChoiceDialog<>(membersList.get(0), membersList);
+        dialog.setTitle("Remove Member");
+        dialog.setHeaderText("Select a member to remove:");
+        dialog.setContentText("Member:");
+
+        Optional<String> result = dialog.showAndWait();
+        result.ifPresent(member -> {
+            membersList.remove(member);
+            updateTeamMembers();
+        });
+    }
+
+    private void updateTeamMembers() {
+        String members = String.join(", ", membersList);
+        teamMembers.setText(members);
+    }
+
+
 }
+
