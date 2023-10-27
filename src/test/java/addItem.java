375c643d7f983782a1cd1554f26bf69140b0728e
Nikita Filin <nikita10filin@gmail.com>
2023-10-28 02:05:58 +0300

    Unit test2 for task3, close #69

--- src/test/java/addItem.java
+++ src/test/java/addItem.java
@@ -1,6 +1,10 @@
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
 import static org.mockito.Mockito.*;
 
+import ee.ut.math.tvt.salessystem.dao.InMemorySalesSystemDAO;
 import ee.ut.math.tvt.salessystem.dao.SalesSystemDAO;
+import ee.ut.math.tvt.salessystem.dataobjects.StockItem;
 import ee.ut.math.tvt.salessystem.logic.Warehouse;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +14,10 @@ public class addItem {
 
     private Warehouse warehouse;
     private SalesSystemDAO dao;
+
+    private Warehouse warehouse1;
+
+    private InMemorySalesSystemDAO dao1;
 
     @Before
     public void setUp() {
@@ -25,5 +33,26 @@ public class addItem {
         inOrder.verify(dao).beginTransaction();
         inOrder.verify(dao).commitTransaction();
 
+    }
+
+    @Test
+    public void testAddingNewItem() {
+
+        dao1 = new InMemorySalesSystemDAO();
+        warehouse1 = new Warehouse(dao1);
+
+        String barcode = "7";
+        int quantity = 10;
+        String name = "New Product";
+        double price = 20;
+
+        warehouse1.addNewProductToWarehouse(barcode, quantity, name, price);
+
+        StockItem addedItem = dao1.findStockItem(Long.parseLong(barcode));
+        System.out.println(addedItem);
+        assertEquals(barcode, String.valueOf(addedItem.getId()));
+        assertEquals(quantity, addedItem.getQuantity());
+        assertEquals(name, addedItem.getName());
+        assertEquals(price, addedItem.getPrice(), 0.001);
     }
 }
