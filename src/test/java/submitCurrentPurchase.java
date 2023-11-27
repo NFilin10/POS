faa4d9c3c0fbadfa707170c9f11f0af18d81e8fd
Artjom Shishkov <artjom.siskov@ut.ee>
2023-11-27 21:49:54 +0200

    Tests fixed?

--- src/test/java/submitCurrentPurchase.java
+++ src/test/java/submitCurrentPurchase.java
@@ -1,19 +1,18 @@
-import ee.ut.math.tvt.salessystem.dataobjects.StockItem;
-import ee.ut.math.tvt.salessystem.dataobjects.SoldItem;
 import ee.ut.math.tvt.salessystem.dao.SalesSystemDAO;
+import ee.ut.math.tvt.salessystem.dataobjects.SoldItem;
+import ee.ut.math.tvt.salessystem.dataobjects.StockItem;
 import ee.ut.math.tvt.salessystem.dataobjects.User;
 import ee.ut.math.tvt.salessystem.logic.ApplicationException;
-import ee.ut.math.tvt.salessystem.logic.NegativePriceException;
 import ee.ut.math.tvt.salessystem.logic.ShoppingCart;
 import ee.ut.math.tvt.salessystem.logic.Warehouse;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 import org.mockito.InOrder;
+import org.mockito.Mockito;
 
 import java.time.LocalTime;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class submitCurrentPurchase {
@@ -65,58 +64,39 @@ public class submitCurrentPurchase {
     }
 
     @Test
-    public void testSubmittingCurrentOrderCreatesPurchase() throws NegativePriceException, ApplicationException {
-        StockItem stockItem = new StockItem(5L, "Burger", "fmefoaenwoinawi",  15.00, 1);
-        warehouse.addNewProductToWarehouse("5", 1, "Burger", 15.0);
-        SoldItem soldItem = new SoldItem(stockItem, 3);
-        cart.addSoldItem(soldItem);
-        User user = new User("User", "Customer", "user", "123" );
-        cart.submitCurrentPurchase(user);
-        verify(dao).findPurchase(soldItem, user);
+    public void testSubmittingCurrentOrderCreatesPurchase() {
+       User dummyUser = new User();
+       cart.submitCurrentPurchase(dummyUser);
+       InOrder inOrder = inOrder(dao);
+       inOrder.verify(dao, times(1)).getPurchaseList(dummyUser);
     }
 
     @Test
-    public void testSubmittingCurrentOrderSavesCorrectTime() throws NegativePriceException, ApplicationException {
-        StockItem stockItem = new StockItem(5L, "Burger", "fmefoaenwoinawi",  15.00, 1);
-        warehouse.addNewProductToWarehouse("5", 1, "Burger", 15.0);
-        SoldItem soldItem = new SoldItem(stockItem, 3);
-        cart.addSoldItem(soldItem);
-        User user = new User("User", "Customer", "user", "123" );
-        cart.submitCurrentPurchase(user);
-        verify(dao).getPurchaseList(user).get(0).getTime();
-        assertEquals(dao.getPurchaseList(user).get(0).getTime().getHour(), LocalTime.now().getHour());
-        assertEquals(dao.getPurchaseList(user).get(0).getTime().getMinute(), LocalTime.now().getMinute());
-        assertEquals(dao.getPurchaseList(user).get(0).getTime().getSecond(), LocalTime.now().getSecond(), 1);
+    public void testSubmittingCurrentOrderSavesCorrectTime() {
+        User dummyUser = new User();
+        cart.submitCurrentPurchase(dummyUser);
+        assertEquals(LocalTime.now(),  dao.getLast10Purchases(dummyUser).get(0).getTime());
     }
 
     @Test
-    public void testCancellingOrder() throws NegativePriceException, ApplicationException {
-        StockItem stockItem1 = new StockItem(6L, "Hot Dog", "ergeri",  8.00, 1);
-        warehouse.addNewProductToWarehouse("6", 1, "Hot Dog", 8.0);
-        SoldItem soldItem1 = new SoldItem(stockItem1, 3);
-        cart.addSoldItem(soldItem1);
+    public void testCancellingOrder() throws ApplicationException {
+        User dummyUser = new User();
+        SoldItem nullItem = new SoldItem();
+        cart.addItem(nullItem);
         cart.cancelCurrentPurchase();
-
-        StockItem stockItem = new StockItem(5L, "Burger", "fmefoaenwoinawi",  15.00, 1);
-        warehouse.addNewProductToWarehouse("5", 1, "Burger", 15.0);
-        SoldItem soldItem = new SoldItem(stockItem, 3);
-        cart.addSoldItem(soldItem);
-        User user = new User("User", "Customer", "user", "123" );
-        cart.submitCurrentPurchase(user);
-
-        assertEquals(dao.getPurchaseList(user).get(0).getItems().get(0), soldItem);
+        SoldItem otherNullItem = new SoldItem();
+        cart.addItem(otherNullItem);
+        cart.submitCurrentPurchase(dummyUser);
+        assertEquals(otherNullItem, dummyUser.getPurchases().get(0).getItems().get(0));
     }
 
     @Test
-    public void testCancellingOrderQuanititiesUnchanged() throws NegativePriceException, ApplicationException {
-        int quantity = 3;
-
-        StockItem stockItem1 = new StockItem(6L, "Hot Dog", "ergeri",  8.00, quantity);
-        warehouse.addNewProductToWarehouse("6", quantity, "Hot Dog", 8.0);
-        SoldItem soldItem1 = new SoldItem(stockItem1, 3);
-        cart.addSoldItem(soldItem1);
+    public void testCancellingOrderQuanititiesUnchanged() throws ApplicationException {
+        StockItem nullItem = new StockItem();
+        nullItem.setQuantity(1);
+        SoldItem soldItem = new SoldItem(nullItem, 1);
+        cart.addItem(soldItem);
         cart.cancelCurrentPurchase();
-
-        assertEquals(quantity, dao.findStockItem(6L).getQuantity());
+        assertEquals(1, nullItem.getQuantity());
     }
 }
